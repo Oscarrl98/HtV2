b40608f069e69fa03ae84b4b5ca2ce26aff929b3
oscarrl98 <oscar_rlp@hotmail.es>
2020-06-07 17:56:05 -0600

    Version SIN patrones

---
+++ Properties/view/Ht3v1App.cs
@@ -0,0 +1,13 @@
+﻿using Ht3v1App.Properties.service;
+
+namespace Ht3v1App.Properties.view
+{
+    public class Ht3v1App
+    {
+        public static void Main(string[] args)
+        {
+            FormatFile nFormat= new FormatFile();
+            nFormat.ConverFile(args);
+        }
+    }
+}
